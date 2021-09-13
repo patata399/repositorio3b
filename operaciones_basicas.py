6ce3cfe2c61365c4fc62bece06768b7b32688c87
patata399 <90343717+patata399@users.noreply.github.com>
2021-09-13 07:28:28 -0500

    Create operaciones_basicas.py

---
+++ operaciones_basicas.py
@@ -0,0 +1,11 @@
+# aqui hacemos operaciones matematicas
+suma = 5 + 2
+resta = suma - 8.5
+m1 = 32
+m2 = 64
+multiplicacion = m1 * m2
+print("este programa acaba de ahcer algunas operaciones matematicas")
+print("la suma dio como resultado")
+print(suma)
+print("la resta dio como resultado" + resta)
+print("la mutiplicacion dio como resultado" + multiplicacion)
