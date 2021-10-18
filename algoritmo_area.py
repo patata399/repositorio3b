213b7cf3e11c04288a075689084ae477f358b416
patata399 <90343717+patata399@users.noreply.github.com>
2021-10-18 12:51:00 +0000

    crear nuevo algoritmo

---
+++ algoritmo_area.py
@@ -0,0 +1,20 @@
+#1 preguntar la altura del cuarto en metros
+print ("cual es la altura del cuarto?")
+#2 definir la altura por el input
+altura = int(input())
+#3 preguntar el ancho del cuarto en metros
+print ("cual es el ancho del caurto?")
+#4 definir el ancho por el input
+ancho = int(input())
+#5 imprimir: ingresaste las siguientes dimensiones: (altura) x (ancho)
+print("ingresaste las siguientes dimensiones", altura, "x", ancho)
+#6 hacer las operaciones altura x ancho
+solucion = altura * ancho
+#7 definir resultado con la solucion
+#8 imprimir: el area es de: (resultado) metros cuadrados
+print("el area es de", solucion, "metros cuadrados")
+#9 (resultado) x 3.2808
+pies = resultado * 3.2808
+#10 definir pies con la solucion
+#10 imprimir: que son (pies) pies cuadrados
+print("que son:", pies, "pies cuadrados")
