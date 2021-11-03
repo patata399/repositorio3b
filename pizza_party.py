f3de89567aab2a21562b0f153f7df7701bc0f721
patata399 <90343717+patata399@users.noreply.github.com>
2021-11-03 18:27:09 +0000

    termine el algoritmo

--- pizza_party.py
+++ pizza_party.py
@@ -3,9 +3,17 @@ personas = 0
 pizzas = 0
 rebanadasxp = 0
 rebanadas = 0
-print("cuantas persoans vienen?")
+sobrante = 0
+reabnadastotal = 0
+print("cuantas personas vienen?")
 personas = input()
 print("cuantas pizzas tienes?")
 pizzas = input()
 print("de cuantas rebanadas?")
 rebanadas = input()
+rebanadastotal = rebanadas * pizzas
+rebanadasxp = rebanadastotal / personas
+sobrante = rebanadas - rebanadasxp
+print("cada persona podra comer", rebanadasxp)
+print("y sobraran", sobrante)
+#fin
