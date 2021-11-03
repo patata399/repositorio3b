8c00b442f7a9f00225e2aa518b6f384d7307f4b1
patata399 <90343717+patata399@users.noreply.github.com>
2021-11-03 04:31:24 +0000

    empezar a crear nuevo algoritmo

---
+++ pizza_party.py
@@ -0,0 +1,11 @@
+#inicio
+personas = 0
+pizzas = 0
+rebanadasxp = 0
+rebanadas = 0
+print("cuantas persoans vienen?")
+personas = input()
+print("cuantas pizzas tienes?")
+pizzas = input()
+print("de cuantas rebanadas?")
+rebanadas = input()
